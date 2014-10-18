4f0b4adbab9a154f022a07d3993e6528fd4d6790
John Browning <WWJohnBrowningDo@gmail.com>
2014-10-18 00:46:24 -0700

    Bigger drives, more CPU and memory.

--- script/update.sh
+++ script/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
+    echo "==> Remove useless packages"
+    yum erase libreoffice-* evolution pidgin ekiga gthumb cheese brasero totem
+
     echo "==> Applying updates"
     yum -y update
 
